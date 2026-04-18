5fd3fd90278189e5c7217572434d90a47ebe72d1
streaku1 <tery581@gmail.com>
2026-04-18 05:37:44 +0300

    Services implementation

---
+++ Services/Implementation/ICarService.cs
@@ -0,0 +1,20 @@
+﻿using Carsweb.Data;
+using Carsweb.Models;
+
+namespace Carsweb.Services.Implementation
+{
+    public class CarService
+    {
+        private readonly AppDbContext _context;
+
+        public CarService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Cars>> GetAllAsync()
+        {
+            return _context.Cars.ToList();
+        }
+    }
+}
